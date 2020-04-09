295ced556ef3c42e4e83b13a46db49bbd8ccbf23
Michael Andrews <michaelcl98@hotmail.com>
2020-04-09 10:42:55 -0300

    Adding 9th april angular class

--- front-end-intelligence/angular/aula4/src/app/app-routing.module.ts
+++ front-end-intelligence/angular/aula4/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
+import { NotFoundComponent } from './error/not-found/not-found.component';
+import { PhotoSearchComponent } from './photos/photo-search/photo-search.component';
+import { PhotosListComponent } from './photos/photos-list/photos-list.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'photos', component: PhotosListComponent},
+  {path: 'photos/search', component: PhotoSearchComponent},
+  {path: '**', component: NotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- front-end-intelligence/angular/aula4/src/app/app.module.ts
+++ front-end-intelligence/angular/aula4/src/app/app.module.ts
@@ -1,10 +1,10 @@
+import { ErrorModule } from './error/error.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PhotosModule } from './photos/photos.module';
-import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -13,8 +13,8 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule,
     PhotosModule,
+    ErrorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
